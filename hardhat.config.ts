aca0be66981dd0dec3378c48825590c93fda2b4e
Wighawag <wighawag@gmail.com>
2020-11-30 21:58:34 +0000

    setup improvement

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,16 +3,7 @@ import {HardhatUserConfig} from 'hardhat/types';
 import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
 import 'hardhat-gas-reporter';
-
-let mnemonic = process.env.MNEMONIC;
-if (!mnemonic) {
-  // FOR DEV ONLY, SET IT IN .env files if you want to keep it private
-  // (IT IS IMPORTANT TO HAVE A NON RANDOM MNEMONIC SO THAT SCRIPTS CAN ACT ON THE SAME ACCOUNTS)
-  mnemonic = 'test test test test test test test test test test test junk';
-}
-const accounts = {
-  mnemonic,
-};
+import {node_url, accounts} from './utils/network';
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -22,28 +13,28 @@ const config: HardhatUserConfig = {
     deployer: 0,
   },
   networks: {
-    coverage: {
-      url: 'http://localhost:5458',
-      accounts,
-    },
     hardhat: {
-      accounts,
+      accounts: accounts(),
     },
     localhost: {
       url: 'http://localhost:8545',
-      accounts,
+      accounts: accounts(),
+    },
+    mainnet: {
+      url: node_url('mainnet'),
+      accounts: accounts('mainnet'),
     },
     rinkeby: {
-      url: 'https://rinkeby.infura.io/v3/' + process.env.INFURA_TOKEN,
-      accounts,
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
     },
-    42: {
-      url: 'https://kovan.infura.io/v3/' + process.env.INFURA_TOKEN,
-      accounts,
+    kovan: {
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
     },
     staging: {
-      url: 'https://goerli.infura.io/v3/' + process.env.INFURA_TOKEN,
-      accounts,
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
     },
   },
   paths: {
---
+++ utils/network.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+export function node_url(networkName: string): string {
+  if (networkName) {
+    const uri = process.env['ETH_NODE_URI_' + networkName.toUpperCase()];
+    if (uri && uri !== '') {
+      return uri;
+    }
+  }
+
+  let uri = process.env.ETH_NODE_URI;
+  if (uri) {
+    uri = uri.replace('{{networkName}}', networkName);
+  }
+  if (!uri || uri === '') {
+    // throw new Error(`environment variable "ETH_NODE_URI" not configured `);
+    return '';
+  }
+  if (uri.indexOf('{{') >= 0) {
+    throw new Error(
+      `invalid uri or network not supported by nod eprovider : ${uri}`
+    );
+  }
+  return uri;
+}
+
+export function getMnemonic(networkName?: string): string {
+  if (networkName) {
+    const mnemonic = process.env['MNEMONIC_' + networkName.toUpperCase()];
+    if (mnemonic && mnemonic !== '') {
+      return mnemonic;
+    }
+  }
+
+  const mnemonic = process.env.MNEMONIC;
+  if (!mnemonic || mnemonic === '') {
+    return 'test test test test test test test test test test test junk';
+  }
+  return mnemonic;
+}
+
+export function accounts(networkName?: string): {mnemonic: string} {
+  return {mnemonic: getMnemonic(networkName)};
+}
