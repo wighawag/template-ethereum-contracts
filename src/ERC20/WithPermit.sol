9ddc17c1f5c9bb22442f9268f9d41555cf0d3862
Wighawag <wighawag@gmail.com>
2021-04-09 10:47:28 +0100

    fix format

--- src/ERC20/WithPermit.sol
+++ src/ERC20/WithPermit.sol
@@ -5,9 +5,8 @@ import "./ERC20Internal.sol";
 import "../Interfaces/IERC2612Standalone.sol";
 
 abstract contract WithPermit is ERC20Internal, IERC2612Standalone {
-    bytes32 internal constant PERMIT_TYPEHASH = keccak256(
-        "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-    );
+    bytes32 internal constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     mapping(address => uint256) internal _nonces;
 
     function DOMAIN_SEPARATOR() public view virtual override returns (bytes32);
@@ -28,13 +27,14 @@ abstract contract WithPermit is ERC20Internal, IERC2612Standalone {
         require(owner != address(0), "INVALID_ZERO_ADDRESS");
 
         uint256 currentNonce = _nonces[owner];
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR(),
-                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentNonce, deadline))
-            )
-        );
+        bytes32 digest =
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentNonce, deadline))
+                )
+            );
         require(owner == ecrecover(digest, v, r, s), "INVALID_SIGNATURE");
         require(deadline == 0 || block.timestamp <= deadline, "TOO_LATE");
 
--- src/ERC20/WithPermitAndFixedDomain.sol
+++ src/ERC20/WithPermitAndFixedDomain.sol
@@ -5,9 +5,8 @@ import "./ERC20Internal.sol";
 import "../Interfaces/IERC2612Standalone.sol";
 
 abstract contract WithPermitAndFixedDomain is ERC20Internal, IERC2612Standalone {
-    bytes32 internal constant PERMIT_TYPEHASH = keccak256(
-        "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-    );
+    bytes32 internal constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
     // solhint-disable-next-line var-name-mixedcase
     bytes32 public immutable override DOMAIN_SEPARATOR;
@@ -44,13 +43,14 @@ abstract contract WithPermitAndFixedDomain is ERC20Internal, IERC2612Standalone 
         require(owner != address(0), "INVALID_ZERO_ADDRESS");
 
         uint256 currentNonce = _nonces[owner];
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentNonce, deadline))
-            )
-        );
+        bytes32 digest =
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    DOMAIN_SEPARATOR,
+                    keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentNonce, deadline))
+                )
+            );
         require(owner == ecrecover(digest, v, r, s), "INVALID_SIGNATURE");
         require(deadline == 0 || block.timestamp <= deadline, "TOO_LATE");
 
