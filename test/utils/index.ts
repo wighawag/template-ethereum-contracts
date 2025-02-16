3a4cba1b4e6cea5bd7110cc507427aa98ec5f5fa
wighawag <wighawag@gmail.com>
2025-02-16 17:46:10 +0000

    TODO test

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -3,10 +3,14 @@ import {loadAndExecuteDeployments} from 'rocketh';
 import '@rocketh/deploy';
 import {context} from '../../deploy/_context.js';
 import {network} from '@ignored/hardhat-vnext';
+// import '@rocketh/deploy';
+// import {context} from '../../deploy/_context.js';
+// import hre from '@ignored/hardhat-vnext';
+// import {loadEnvironmentFromHardhat} from 'hardhat3-rocketh/helpers';
 
 export async function deployAll() {
+	// const env = await loadEnvironmentFromHardhat({hre, context});
 	const {provider} = await network.connect();
-
 	const env = await loadAndExecuteDeployments(
 		{
 			provider: provider as EIP1193GenericRequestProvider,
