c710b5507613f48a46bbbe6d98e87411b634a49e
wighawag <wighawag@gmail.com>
2025-03-12 10:35:47 +0000

    comment

--- deploy/001_deploy_greetings_registry.ts
+++ deploy/001_deploy_greetings_registry.ts
@@ -1,9 +1,15 @@
+// execute is needed to register your script
 import {execute} from 'rocketh';
+// here we add the `@rocketh/deploy` extension that let you use the deploy function (hardhat-deploy v2 is fully modular)
 import '@rocketh/deploy';
+// here we import the context, the convention is to import it from a file named `_context.ts`
 import {context} from './_context.js';
 
 export default execute(
+	// we pass the context to the "execute" function
+	// it will transform it while keeping type safety (in particular namedAccounts)
 	context,
+	// then you pass in your function that can do whatever it wants
 	async ({deploy, namedAccounts, artifacts}) => {
 		const {deployer} = namedAccounts;
 
@@ -13,5 +19,6 @@ export default execute(
 			args: [''],
 		});
 	},
+	// finally you can pass tags and dependencies (no dependencies here)
 	{tags: ['GreetingsRegistry', 'GreetingsRegistry_deploy']},
 );
