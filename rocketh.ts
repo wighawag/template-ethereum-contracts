d6fd925953e07823a6a362c0628c2f8447662284
wighawag <wighawag@gmail.com>
2025-08-21 22:43:45 +0530

    show getHardhatConnection

--- rocketh.ts
+++ rocketh.ts
@@ -47,7 +47,8 @@ export const config = {
 import * as deployFunctions from '@rocketh/deploy'; // this one provide a deploy function
 import * as readExecuteFunctions from '@rocketh/read-execute'; // this one provide read,execute functions
 import * as deployProxyFunctions from '@rocketh/proxy'; // this one provide a deployViaProxy function that let you declaratively deploy proxy based contracts
-const functions = {...deployFunctions, ...readExecuteFunctions, ...deployProxyFunctions};
+import {getHardhatConnection} from 'hardhat-deploy';
+const functions = {...deployFunctions, ...readExecuteFunctions, ...deployProxyFunctions, getHardhatConnection};
 // ------------------------------------------------------------------------------------------------
 // we re-export the artifacts, so they are easily available from the alias
 import artifacts from './generated/artifacts.js';
