86ce52eafb80b7ecde5c5c7bd6fc955f453fbc90
wighawag <wighawag@gmail.com>
2025-12-15 14:30:09 +0000

    fix

--- rocketh/environment.ts
+++ rocketh/environment.ts
@@ -1,10 +1,10 @@
 import {extensions} from './deploy.js';
 import type {Extensions, Accounts, Data} from './deploy.js'
-import {setupEnvironment} from 'rocketh';
+import {setupEnvironmentFromFiles} from 'rocketh';
 import {setupHardhatDeploy} from 'hardhat-deploy/helpers';
 
 // useful for test and scripts, uses file-system
-const {loadAndExecuteDeployments} = setupEnvironment<Extensions,Accounts,Data>(extensions);
+const {loadAndExecuteDeployments} = setupEnvironmentFromFiles<Extensions,Accounts,Data>(extensions);
 const {loadEnvironmentFromHardhat} = setupHardhatDeploy<Extensions,Accounts,Data>(extensions)
 
 export {loadEnvironmentFromHardhat, loadAndExecuteDeployments};
